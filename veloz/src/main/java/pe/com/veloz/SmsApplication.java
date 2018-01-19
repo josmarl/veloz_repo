07d2671d8dd2c1603ed31e09f5319bd45b844b31
Your Name <you@example.com>
2018-01-19 04:49:11 -0500

    s

--- veloz/src/main/java/pe/com/veloz/SmsApplication.java
+++ veloz/src/main/java/pe/com/veloz/SmsApplication.java
@@ -16,7 +16,7 @@ import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
