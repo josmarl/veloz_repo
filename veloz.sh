0d152614f8ba99120ef188c5a51510b7e31a8990
josmarl <josmarl567@gmail.com>
2019-02-07 15:48:59 -0500

    bash

--- veloz.sh
+++ veloz.sh
@@ -1,1 +1,9 @@
+TIMEOUT /T 3600 /NOBREAK &
+"C:\xampp\mysql_start.bat" &
+java -jar "E:\dev\proyectos\veloz_repo\veloz\target\veloz-1.0.jar" &
+
+
+
+
 "C:\Program Files\Mozilla Firefox\firefox.exe" localhost:8088
+
